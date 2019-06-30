abb68d7c6688b27b34290d7b25759d5b3e104d90
Humaniuc <humancoria@gmail.com>
2019-06-30 10:56:50 +0300

    ConnectToDatabase

---
+++ InsertPublisherApp/InsertPublisherApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsertPublisherApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-C6SIFK8;Initial Catalog=BookCase;Integrated Security=True");
+
+        }
+    }
+}

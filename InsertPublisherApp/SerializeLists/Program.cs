eb021cf3b38ef5cbe3a1d239659c5a2debee381f
Humaniuc <humancoria@gmail.com>
2019-07-02 12:10:49 +0300

    Serializer Class

--- InsertPublisherApp/SerializeLists/Program.cs
+++ InsertPublisherApp/SerializeLists/Program.cs
@@ -1,14 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AdoConnection;
 using Entities;
 using Newtonsoft;
-using Newtonsoft.Json;
-using System.IO;
-using System.Xml.Serialization;
 using System.Collections;
 
 namespace SerializeLists
@@ -21,24 +17,8 @@ namespace SerializeLists
             BookRepo bookRepo = new BookRepo(connection);
 
             List<Book> books = bookRepo.LoadAllBooks();
-
-            if (File.Exists("Books.json"))
-            {
-                File.Delete("Books.json");
-            }
-            foreach (Book book in books)
-            {
-                var jSonSerializeRezult = JsonConvert.SerializeObject(book);
-                
-                File.AppendAllText("Books.json", jSonSerializeRezult + Environment.NewLine);
-            }
-
-            using (var writer = new System.IO.StreamWriter("Books.xml"))
-            {
-                var serializer = new XmlSerializer(books.GetType());
-                serializer.Serialize(writer, books);
-                writer.Flush();
-            }
+            Serializer.SerializeJSon(books);
+            Serializer.SerializeXML(books);
         }
     }
 }
---
+++ InsertPublisherApp/SerializeLists/Serializer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Entities;
+using Newtonsoft.Json;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace SerializeLists
+{
+    public class Serializer
+    {
+        public static void SerializeXML(List<Book> books)
+        {
+            using (var writer = new System.IO.StreamWriter("Books.xml"))
+            {
+                var serializer = new XmlSerializer(books.GetType());
+                serializer.Serialize(writer, books);
+                writer.Flush();
+            }
+        }
+
+        public static void SerializeJSon(List<Book> books)
+        {
+            if (File.Exists("Books.json"))
+            {
+                File.Delete("Books.json");
+            }
+            foreach (Book book in books)
+            {
+                var jSonSerializeRezult = JsonConvert.SerializeObject(book);
+
+                File.AppendAllText("Books.json", jSonSerializeRezult + Environment.NewLine);
+            }
+        }
+    }
+}
